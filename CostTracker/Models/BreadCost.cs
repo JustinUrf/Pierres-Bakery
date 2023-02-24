1f13ad48bbf9f9ee93ec099380802f620406b996
Justin Lee <lee.justin001126@gmail.com>
2023-02-24 08:41:05 -0800

    add basic models for Bakery.Solutions

---
+++ CostTracker/Models/BreadCost.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace BreadCost.Models 
+{
+  public class CalculateBread
+  {
+
+  }
+}
---
+++ CostTracker/Program.cs
@@ -0,0 +1,9 @@
+using System;
+using BreadCost.Models;
+using PastryCost.Models;
+
+namespace CostTracker {
+  class Program {
+    
+  }
+}
