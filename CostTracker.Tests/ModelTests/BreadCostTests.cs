1ccae1ec2837c0a0c379819a61c75facd07e4336
Justin Lee <lee.justin001126@gmail.com>
2023-02-24 10:01:18 -0800

    created a good failing test

--- CostTracker.Tests/ModelTests/BreadCostTests.cs
+++ CostTracker.Tests/ModelTests/BreadCostTests.cs
@@ -11,7 +11,16 @@ namespace CostTracker.Tests
     {
       CalculateBread newBread = new CalculateBread();
       newBread.SetBreadBought(4);
-      Assert.AreEqual(4, newBread._breadsBought);
+      Assert.AreEqual(4, newBread.BreadsBought);
+    }
+
+    [TestMethod]
+    public void TotalCost_MultiplyGivenInstancesOfBread_TotalAmount()
+    {
+      CalculateBread newBread = new CalculateBread();
+      newBread.SetBreadBought(5);
+      newBread.MultiplyBread();
+      Assert.AreEqual(25, newBread.TotalCost);
     }
   }
 }
